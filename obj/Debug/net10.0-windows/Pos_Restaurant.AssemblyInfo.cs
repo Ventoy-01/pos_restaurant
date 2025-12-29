cd6d8dcf44d696a3a12aafaf49f4b3866c3e7c9b
vinchy19 <cherenfanyvinchy@gmail.com>
2025-12-28 23:13:27 -0500

    change

--- obj/Debug/net10.0-windows/Pos_Restaurant.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/Pos_Restaurant.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pos_Restaurant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+046e6084b2cb51aec5b980a1b24002718a09b20d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f41c931753333a28d85fbcd9abca09c6c211b65")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pos_Restaurant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pos_Restaurant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
